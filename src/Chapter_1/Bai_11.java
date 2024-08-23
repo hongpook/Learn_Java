69c821165cb18a89d1968cc5c159d50b8775eef6
hongpook <hongphuc12@gmail.com>
2024-08-23 17:12:32 +0700

    commit chapter 1

--- src/Chapter_1/Bai_11.java
+++ src/Chapter_1/Bai_11.java
@@ -13,11 +13,35 @@ public class Bai_11 {
 			Viết chương trình hiển thị dân số của từng năm trong 5 năm tiếp theo. 
 			Giả sử rằng dân số hiện nay là 312.032.486 người, một năm có 365 ngày. 
 			Gợi ý: Trong Java, nếu hai số nguyên thực hiện phép chia thì kết quả là một số nguyên. Phần phân đoạn được cắt ngắn. 
-			Ví dụ: 5/4 là 1 (không phải 1,25) và 10/4 là 2 (không phải 2,5). Để có được kết quả chính xác 
-			với phần phân số, một trong các giá trị liên quan đến phép chia phải là số 
-			bằng dấu thập phân. Ví dụ: 5,0/4 là 1,25 và 10/4,0 là 2,5.
+			Ví dụ: 5/4 là 1 (không phải 1,25) và 10/4 là 2 (không phải 2,5). Để có được kết quả chính xác với phần phân số, 
+			một trong các giá trị liên quan đến phép chia phải là số bằng dấu thập phân. Ví dụ: 5,0/4 là 1,25 và 10/4,0 là 2,5.
+			
+			dân số hiện nay: 312.032.486 người
+			1 năm có: 365 ngày
+			cứ: 
+			7s = 1 ca sinh nở
+			13s = 1 người chết
+			45s = 2 người nhập cư mới
 		 */
-
+		
+		
+		long currentPopulation = 312032486;
+		long secondsInYear = 365 * 24 * 60 * 60;
+		int years = 5;
+		
+		int born = 7;
+		int dead = 13;
+		int immigration = 45;
+		
+		long bornPerYear = secondsInYear / born;
+		long deadPerYear = secondsInYear / dead;
+		long immigrationPerYear = secondsInYear / immigration;
+		
+		System.out.println("Dân số hiện tại là: " + currentPopulation);
+		for (int year = 1; year <= years ; year ++) {
+			currentPopulation = currentPopulation + bornPerYear - deadPerYear + immigrationPerYear;
+			System.out.println("Dân số trong " + year + " tiếp theo là: " + currentPopulation + " người");
+		}
 	}
 
 }
--- src/Chapter_1/Bai_2.java
+++ src/Chapter_1/Bai_2.java
@@ -4,7 +4,7 @@ public class Bai_2 {
 
 	public static void main(String[] args) {
 		int n = 5;
-		for (int i =1; i<=5; i++) {
+		for (int i =1; i<=n; i++) {
 			System.out.println("I love Java");
 		}
 	}
